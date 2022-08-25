0466b5bd2eeb82ef5e33e5eba63bda0105cf2dd1
Chimexx <mezie.cjo@gmail.com>
2022-08-25 21:08:05 +0100

    Implemented Comms with notification service via RabbitMq

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+/* Creating rabbitmq service that can be used in other modules. */
+const RabbitMQService = {
+  provide: 'NOTIFY_SERVICE',
+  useFactory: (configService: ConfigService) => {
+    return ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: [configService.get<string>('RABBITMQ_URL')],
+        queue: configService.get<string>('RABBITMQ_QUEUE'),
+        queueOptions: {
+          durable: false,
+        },
+      },
+    });
+  },
+  inject: [ConfigService],
+};
+@Global()
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, RabbitMQService],
+  exports: [RabbitMQService],
 })
 export class AppModule {}
