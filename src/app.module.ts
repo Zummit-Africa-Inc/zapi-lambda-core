6cc1d2d357147386ff810d1da88bd2c789d5a731
Oladimeji Ibitoye <stephen_ibitoye@yahoo.com>
2022-08-29 10:27:12 +0100

    resolving the conflict

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EndpointsModule } from './endpoints/endpoints.module';
+import { SubscriptionModule } from './subscription/subscription.module';
 
 @Module({
-  imports: [EndpointsModule],
+  imports: [EndpointsModule, SubscriptionModule],
   controllers: [AppController],
   providers: [AppService],
 })
