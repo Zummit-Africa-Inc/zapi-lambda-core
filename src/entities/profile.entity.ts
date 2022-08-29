059cff41759662d08e295e71039e852aba57a9db
Ayinde John <lolaayinde@gmail.com>
2022-08-29 19:05:05 +0100

    updated profile entity

--- src/entities/profile.entity.ts
+++ src/entities/profile.entity.ts
@@ -1,17 +1,24 @@
-import { SharedEntity } from "../common/model/sharedEntity";
-import { Column, Entity } from "typeorm";
+import { SharedEntity } from '../common/model/sharedEntity';
+import { Column, Entity, JoinColumn, OneToMany } from 'typeorm';
+import { Subscription } from './subscription.entity';
 
 @Entity()
 export class Profile extends SharedEntity {
-    @Column()
-    email: string;
+  @Column()
+  email: string;
 
-    @Column()
-    userId: string
+  @Column()
+  userId: string;
 
-    @Column({ nullable: true})
-    picture: string
+  @Column({ nullable: true })
+  picture: string;
 
-    @Column('text', {array: true, nullable: true, default:[]})
-    subscription: string[]
+  @Column('text', { array: true, nullable: true, default: [] })
+  subscriptions: string[];
+
+  @OneToMany(() => Subscription, (subscription) => subscription.profile, {
+    onDelete: 'SET NULL',
+  })
+  @JoinColumn({ name: 'subscriptions' })
+  subscription: Subscription[];
 }
