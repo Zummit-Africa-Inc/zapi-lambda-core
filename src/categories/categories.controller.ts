db513f416b546ab80407bf01a9ad17f1b3b02579
Tijani Ayomide <ayomidetijani6@gmail.com>
2022-08-30 19:09:00 +0100

    Creating category and finding all category

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,34 +1,40 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { ZaLaResponse } from '../common/helpers/response';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
-  constructor(private readonly categoriesService: CategoriesService) {}
+  constructor(private readonly categoryService: CategoriesService) {}
 
-  @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoriesService.create(createCategoryDto);
+  @Post('/create')
+  @ApiOperation({summary: 'Create a new category'})
+  async create(@Body() createCategoryDto: CreateCategoryDto) {
+    const category = await this.categoryService.createNewCategory(createCategoryDto);
+    return ZaLaResponse.Ok(category, 'Category created', '201')
   }
 
   @Get()
-  findAll() {
-    return this.categoriesService.findAll();
+  @ApiOperation({summary: 'Get all available category'})
+  async findAll(){
+    const allCategories = this.categoryService.findAllCategory();
+    return ZaLaResponse.Ok(allCategories, 'Ok', '200')
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.categoriesService.findOne(+id);
+    return this.categoryService.findOne(+id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return this.categoriesService.update(+id, updateCategoryDto);
+    return this.categoryService.update(+id, updateCategoryDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.categoriesService.remove(+id);
+    return this.categoryService.remove(+id);
   }
 }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,15 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Category } from '../entities/category.entity';
+import { Repository } from 'typeorm';
+import { ZaLaResponse } from 'src/common/helpers/response';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepo: Repository<Category>
+  ) {}
+
+  async createNewCategory(createCategoryDto: CreateCategoryDto):Promise<Category> {
+    try{
+      const category = await this.categoryRepo.findOne({
+        where: { name: createCategoryDto.name},
+      })
+
+      if(category) {
+        throw new BadRequestException(
+          ZaLaResponse.BadRequest(
+            'Existing Value',
+            `A category with name ${createCategoryDto.name} already exist`,
+            '400'
+          )
+        )
+      }
+
+      const newCategory = this.categoryRepo.create({
+        ...createCategoryDto,
+      });
+      const savedCategory = await this.categoryRepo.save(newCategory);
+      return savedCategory;
+    }
+    catch(err){
+      throw new BadRequestException(
+        ZaLaResponse.BadRequest(err.name, err.message, err.status)
+      )
+    }
   }
 
-  findAll() {
-    return `This action returns all categories`;
+  async findAllCategory(): Promise<Category[]> {
+    try{
+      const category = await this.categoryRepo.find({
+        select: { id: true, name: true, description: true}
+      })
+      return category;
+    }
+    catch(err){
+      throw new BadRequestException(
+        ZaLaResponse.BadRequest(err.name, err.message, err.status)
+      )
+    }
   }
 
   findOne(id: number) {
