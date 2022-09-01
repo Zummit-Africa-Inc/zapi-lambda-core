4e3ca343d5c33cda45bb22098ea81b9beb40a6f9
alahirajeffrey <alahirajeffrey@gmail.com>
2022-09-01 15:03:12 +0100

    added service to find all apis in a category

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ZaLaResponse } from 'src/common/helpers/response';
+import { Category } from 'src/entities/category.entity';
+import { Repository } from 'typeorm';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepo : Repository<Category>) {}
+
   create(createCategoryDto: CreateCategoryDto) {
     return 'This action adds a new category';
   }
@@ -22,5 +30,19 @@ export class CategoriesService {
 
   remove(id: number) {
     return `This action removes a #${id} category`;
+  }
+
+  async getAllApis(categoryId: string){
+    //check if category exists
+    const categoryExists = await this.categoryRepo.findOne({where:{id:categoryId}})
+    if(!categoryExists){
+      throw ZaLaResponse.NotFoundRequest(
+        "Not found",
+        "Category not found",
+        "404"
+      )
+    }
+    //return all apis in the category
+    return categoryExists.api
   }
 }
