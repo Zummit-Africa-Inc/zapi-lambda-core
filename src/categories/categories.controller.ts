11ec2e03569916df933990eae2500b1a0c8d46ac
alahirajeffrey <alahirajeffrey@gmail.com>
2022-09-01 15:06:21 +0100

    added controller to get all apis in a category

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ZaLaResponse } from 'src/common/helpers/response';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
+@ApiTags('Categories')
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
@@ -17,11 +20,17 @@ export class CategoriesController {
     return this.categoriesService.findAll();
   }
 
+  @Get('getAllApis:id')
+  @ApiOperation({summary:"get all apis in a particular category"})
+  findAllApis(@Param('categoryId') categoryId: string) {
+    return this.categoriesService.getAllApis(categoryId);
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.categoriesService.findOne(+id);
   }
-
+  
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
     return this.categoriesService.update(+id, updateCategoryDto);
@@ -31,4 +40,7 @@ export class CategoriesController {
   remove(@Param('id') id: string) {
     return this.categoriesService.remove(+id);
   }
+
+
+  
 }
