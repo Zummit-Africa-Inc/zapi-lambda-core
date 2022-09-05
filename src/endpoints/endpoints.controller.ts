4ac8810706696c4b115aba1b8c6f4b59222ea3e1
alahirajeffrey <alahirajeffrey@gmail.com>
2022-09-05 11:52:07 +0100

    added route to get all endpoints of an api

--- src/endpoints/endpoints.controller.ts
+++ src/endpoints/endpoints.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { FindApiEndpoints } from './dto/find-api-endpoints.dto';
+import { EndpointsService } from './endpoints.service';
 
+@ApiTags('endpoints')
 @Controller('endpoints')
-export class EndpointsController {}
+export class EndpointsController {
+    constructor(
+        private readonly endpointsService : EndpointsService
+    ){}
+
+    @Get('api:apiId')
+    @ApiOperation({summary: "Get all endpoints of an api"})
+    async getApiEndpoints(@Param("apiId") dto : FindApiEndpoints ){
+        return await this.endpointsService.getAllApiEndpoints(dto)
+    }
+}
--- src/endpoints/endpoints.service.ts
+++ src/endpoints/endpoints.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ZaLaResponse } from 'src/common/helpers/response';
+import { Endpoint } from 'src/entities/endpoint.entity';
+import { Repository } from 'typeorm';
+import { FindApiEndpoints } from './dto/find-api-endpoints.dto';
+
 
 @Injectable()
-export class EndpointsService {}
+export class EndpointsService {
+    constructor(
+        @InjectRepository(Endpoint)
+        private readonly endpointRepo : Repository<Endpoint>
+    ){}
+
+    async getAllApiEndpoints(dto: FindApiEndpoints){
+        try {
+            //check if api exists in Endpoint table
+            const apiExists = await this.endpointRepo.find({where : { apiId : dto.apiId }})
+            if(!apiExists){
+                throw new NotFoundException(
+                    ZaLaResponse.NotFoundRequest(
+                        "Not Found",
+                        "API does not exists",
+                        "404"
+                    )
+                )
+            }
+            //return endpoints
+            return apiExists    
+        } catch (error) {
+            throw new BadRequestException(
+                ZaLaResponse.BadRequest(
+                    error.name,
+                    error.message,
+                    error.status
+                )
+            )
+        }
+    }
+}
