9f6de6685aa1893e54803b1aa458c3feb10ab541
Chimexx <mezie.cjo@gmail.com>
2022-09-06 16:36:47 +0100

    RabbitMq test on production

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,22 +1,37 @@
-import { Controller, Post, Body, Get, ParseUUIDPipe, Param} from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  ParseUUIDPipe,
+  Param,
+} from '@nestjs/common';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { ProfileService } from './profile.service';
 import { Profile } from '../entities/profile.entity';
 import { Ok, ZaLaResponse } from '../common/helpers/response';
+import { EventPattern } from '@nestjs/microservices';
 
 @Controller('profile')
 export class ProfileController {
-    constructor(private profileService: ProfileService) {}
+  constructor(private profileService: ProfileService) {}
 
-    @Post('/create')
-    async createProfile(@Body() body: CreateProfileDto): Promise<Ok<Profile>> {
-        const userProfile = await this.profileService.createprofile(body)
-        return ZaLaResponse.Ok(userProfile, 'Profile created', 201)
-    }
+  @Post('/create')
+  async createProfile(@Body() body: CreateProfileDto): Promise<Ok<Profile>> {
+    const userProfile = await this.profileService.createprofile(body);
+    return ZaLaResponse.Ok(userProfile, 'Profile created', 201);
+  }
 
-    @Get('/:id')
-    async getOne(@Param('id', new ParseUUIDPipe()) id: string): Promise<Ok<Profile>> {
-        const profile = await this.profileService.getone(id)
-        return ZaLaResponse.Ok(profile, 'Ok', 200)
-    }
+  @Get('/:id')
+  async getOne(
+    @Param('id', new ParseUUIDPipe()) id: string,
+  ): Promise<Ok<Profile>> {
+    const profile = await this.profileService.getone(id);
+    return ZaLaResponse.Ok(profile, 'Ok', 200);
+  }
+
+  @EventPattern('test')
+  async testProd(@Body() body: any) {
+    console.log(body);
+  }
 }
