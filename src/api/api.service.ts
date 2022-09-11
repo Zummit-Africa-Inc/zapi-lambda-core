35014933f86285f7596a0ab314f6f4631fe45418
Adebiyi Oluwarotimi <66860398+danifara3@users.noreply.github.com>
2022-09-11 23:32:47 +0100

    Endpoint to get all APIs belonging to a user

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ZaLaResponse } from 'src/common/helpers/response';
+import { Api } from 'src/entities/api.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ApiService {}
+export class ApiService {
+  constructor(
+    @InjectRepository(Api)
+    private readonly apiRepository: Repository<Api>,
+  ) {}
+  /**
+   * @param {string} profileId - The id of the user who is trying to get his or her api list.
+   * checks if user has an api created from query result.
+   * @returns The getUserApis method returns a promise of unique api created by the user(profileId).
+   */
+
+  async getUserApis(profileId: string): Promise<Api[]> {
+    try {
+      const userApis = await this.apiRepository.find({ where: { profileId } });
+      if (userApis.length === 0) {
+        throw new NotFoundException(
+          ZaLaResponse.NotFoundRequest(
+            'Not Found',
+            'User has no api created.',
+            '404',
+          ),
+        );
+      }
+      return userApis;
+    } catch (error) {
+      throw new BadRequestException(
+        ZaLaResponse.BadRequest('Internal Server error', error.message, '500'),
+      );
+    }
+  }
+}
