f1271fe74232f99a5104b140077cc3623d77e1ee
Chimexx <mezie.cjo@gmail.com>
2022-09-14 11:42:17 +0100

    Branch Update and migration cleanup

--- src/endpoints/endpoints.controller.ts
+++ src/endpoints/endpoints.controller.ts
@@ -1,20 +1,20 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { ZaLaResponse } from 'src/common/helpers/response';
 import { EndpointsService } from './endpoints.service';
-import { ZaLaResponse } from './../common/helpers/response/Response';
-import { Ok } from 'src/common/helpers/response';
+import { Ok, ZaLaResponse } from 'src/common/helpers/response';
 import { Endpoint } from 'src/entities/endpoint.entity';
-import { CreateEndpointDto } from './dto/create-endpoint.dto';
 
 @ApiTags('endpoints')
 @Controller('endpoints')
 export class EndpointsController {
   constructor(private readonly endpointsService: EndpointsService) {}
-  
+
   @Get('api:apiId')
   @ApiOperation({ summary: 'Get all endpoints of an api' })
-  async getApiEndpoints(@Param('apiId') apiId: string) {
-    return await this.endpointsService.getAllApiEndpoints(apiId);
+  async getApiEndpoints(
+    @Param('apiId') apiId: string,
+  ): Promise<Ok<Endpoint[]>> {
+    const endpoints = await this.endpointsService.getAllApiEndpoints(apiId);
+    return ZaLaResponse.Ok(endpoints, 'Ok', 200);
   }
 }
