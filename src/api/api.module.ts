4d0007634f5ab81175d4ddd097e9955673c124f4
Chimexx <mezie.cjo@gmail.com>
2022-09-14 11:47:18 +0100

    updated api module

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Api } from 'src/entities/api.entity';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Api])],
   controllers: [ApiController],
-  providers: [ApiService]
+  providers: [ApiService],
 })
 export class ApiModule {}
