f48f7e48fd25e3d16883629c812aa3d2d04a81e2
Tijani Ayomide <ayomidetijani6@gmail.com>
2022-09-14 04:51:31 +0100

    add and delete api

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Api } from '../entities/api.entity'
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Api,])
+  ],
   controllers: [ApiController],
   providers: [ApiService]
 })
--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,4 +1,93 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ZaLaResponse } from 'src/common/helpers/response';
+import { Repository } from 'typeorm';
+import { Api } from '../entities/api.entity';
+import { CreateApiDto } from './dto/create-api.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class ApiService {}
+export class ApiService {
+    constructor(
+        @InjectRepository(Api)
+        private readonly apiRepo: Repository<Api>
+    ){}
+
+    async createApi(createApiDto: CreateApiDto, profileId: string) {
+        try {
+            const apiExist = await this.apiRepo.findOne({
+                where: { name: createApiDto.name, }
+            });
+
+            if (apiExist) {
+                throw new BadRequestException(
+                    ZaLaResponse.BadRequest(
+                        'Existing values',
+                        'An api with with this name already exist... try another name',
+                    ),
+                );
+            }
+            const uniqueApiSecretKey = uuid();
+            const newApi = this.apiRepo.create({
+                ...createApiDto,
+                profileId,
+                secretKey: uniqueApiSecretKey,
+            });
+            const saveApi = await this.apiRepo.save(newApi);
+            return saveApi;
+        }
+        catch(err) {
+            throw new BadRequestException(
+                ZaLaResponse.BadRequest('Internal Server error', err.message, '500'),
+            );
+        }
+    }
+
+    async verify(apiId: string, profileId: string) {
+        try {
+            const api = await this.apiRepo.findOne({ where: { id: apiId }});
+            if(!api) {
+                throw new NotFoundException(
+                    ZaLaResponse.NotFoundRequest(
+                        'Not found',
+                        'The api does not exist',
+                        '404',
+                    ),
+                );
+            } else {
+                const status = api.profileId === profileId ? true: false;
+                return status;
+            }
+        }
+        catch(err) {
+            throw new BadRequestException(
+                ZaLaResponse.BadRequest('Internal Server error', err.message, '500'),
+            );
+        }
+    }
+
+    async deleteApi(apiId: string, profileId: string) {
+        try {
+            const apiExist = await this.apiRepo.findOne({ where: { id: apiId }});
+            const isOwner = await this.verify(apiId, profileId);
+
+            if(isOwner === false) {
+                throw new NotFoundException(
+                    ZaLaResponse.BadRequest('Forbidden', 'Unauthorized action', '403'),
+                );
+            }
+
+            if(apiExist && isOwner === true) {
+                return await this.apiRepo.remove(apiExist);
+            }
+            throw new NotFoundException(
+                ZaLaResponse.NotFoundRequest('Not Found', 'Api does not exist', '404'),
+            ); 
+        }
+        catch(err) {
+            throw new BadRequestException(
+                ZaLaResponse.BadRequest('Internal Server error', err.message, '500'),
+            );
+        }
+    }
+}
