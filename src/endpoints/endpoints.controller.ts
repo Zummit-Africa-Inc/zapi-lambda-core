08fcb5c8a87e258ac8572838ec7a3910acbbe5a7
Adebiyi Oluwarotimi <66860398+danifara3@users.noreply.github.com>
2022-09-14 12:09:46 +0100

    fixed api.module issue

--- src/endpoints/endpoints.controller.ts
+++ src/endpoints/endpoints.controller.ts
@@ -1,11 +1,7 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { ZaLaResponse } from 'src/common/helpers/response';
 import { EndpointsService } from './endpoints.service';
-import { ZaLaResponse } from './../common/helpers/response/Response';
-import { Ok } from 'src/common/helpers/response';
-import { Endpoint } from 'src/entities/endpoint.entity';
-import { CreateEndpointDto } from './dto/create-endpoint.dto';
+
 
 @ApiTags('endpoints')
 @Controller('endpoints')
