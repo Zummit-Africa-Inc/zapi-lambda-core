0878ccee9703c09fa10a85d5680dc4fa2032e3af
Ayinde John <lolaayinde@gmail.com>
2022-09-16 09:24:28 +0100

    create DTO file showing all the optionaly columns that can be updated on the API table

---
+++ src/api/dto/update-api.dto.ts
@@ -0,0 +1,37 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+import { Visibility } from 'src/common/enums/visibility.enum';
+import { CreateApiDto } from './create-api.dto';
+
+export class UpdateApiDto extends PartialType(CreateApiDto) {
+  @ApiPropertyOptional()
+  name: string;
+
+  @ApiPropertyOptional()
+  description: string;
+
+  @ApiPropertyOptional()
+  base_url: string;
+
+  @ApiPropertyOptional()
+  about: string;
+
+  @ApiPropertyOptional()
+  categoryId: string;
+
+  @IsString()
+  @ApiPropertyOptional()
+  logo_url: string;
+
+  @IsString()
+  @ApiPropertyOptional()
+  api_website: string;
+
+  @IsString()
+  @ApiPropertyOptional()
+  term_of_use: string;
+
+  @ApiPropertyOptional()
+  visibility: Visibility;
+}
