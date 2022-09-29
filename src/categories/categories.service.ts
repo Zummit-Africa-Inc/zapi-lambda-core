e40883919696d7512eebef0cec1e789f4c59fdd5
Ayinde John <lolaayinde@gmail.com>
2022-09-29 14:54:34 +0100

    cleared category ID checks

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,10 +1,9 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable,  } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ZaLaResponse } from 'src/common/helpers/response';
 import { Category } from 'src/entities/category.entity';
 import { Repository } from 'typeorm';
 import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoriesService {
@@ -54,31 +53,12 @@ export class CategoriesService {
     }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
-  }
-
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} category`;
-  }
 
   async getAllApis(categoryId: string){
     try {
       //check if category exists
       const categoryExists = await this.categoryRepo.findOne({where:{id:categoryId}})
-      if(!categoryExists){
-        throw new NotFoundException(
-          ZaLaResponse.NotFoundRequest(
-            "Not found",
-            "Category not found",
-            "404"
-        )
-      ) 
-    }
+     
       //return all apis in the category
       return categoryExists.api
       
