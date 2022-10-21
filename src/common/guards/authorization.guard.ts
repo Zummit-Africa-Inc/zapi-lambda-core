449557a01ca950868634c1219c577d071b66c15a
Chimexx <mezie.cjo@gmail.com>
2022-10-21 01:00:28 +0100

    Created Authorization guard

---
+++ src/common/guards/authorization.guard.ts
@@ -0,0 +1,41 @@
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { ZaLaResponse } from '../helpers/response';
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    try {
+      const request = context.switchToHttp().getRequest();
+      const requestProfileId = request.query['profileId'];
+      const profileId = request.profileId;
+
+      if (requestProfileId !== profileId) {
+        throw new ForbiddenException(
+          ZaLaResponse.BadRequest(
+            'Authorization Error',
+            'Authorization required to perform this action',
+            '401',
+          ),
+        );
+      }
+
+      return true;
+    } catch (error) {
+      if (error) {
+        throw new BadRequestException(
+          ZaLaResponse.BadRequest(
+            'Internal Server Error',
+            error.message,
+            '500',
+          ),
+        );
+      }
+    }
+  }
+}
