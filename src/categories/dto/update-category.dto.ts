f338b8e675ad9ecf55f58e28d99ee76049451988
alahirajeffrey <alahirajeffrey@gmail.com>
2022-10-31 11:09:34 +0100

    edited update category dto

--- src/categories/dto/update-category.dto.ts
+++ src/categories/dto/update-category.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsOptional } from 'class-validator';
 import { CreateCategoryDto } from './create-category.dto';
 
-export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
+export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
+    @IsOptional()
+    @ApiProperty()
+    name: string;
+
+    @IsOptional()
+    @ApiProperty()
+    description: string;
+}
