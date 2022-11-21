6f586ffbc1d1d24947ec2f2b002a5a5c1c5f5632
alahirajeffrey <alahirajeffrey@gmail.com>
2022-11-21 13:38:54 +0100

    updated dto

--- src/api/dto/add-api-rating.dto.ts
+++ src/api/dto/add-api-rating.dto.ts
@@ -1,11 +1,11 @@
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
-import { IsNotEmpty, IsOptional, IsString, Max, Min } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString, Max, Min } from "class-validator";
 export class ApiRatingDto {
     @ApiPropertyOptional()
-    @IsString()
     review? : string
 
     @ApiProperty()
+    @IsNumber()
     @IsNotEmpty()
     @Min(1, {message: "You cannot rate an api less than 1"})
     @Max(5, {message: 'You cannot rate an api more than 5'})
