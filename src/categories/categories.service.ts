464c939efc3ba1419a88c4b66dbf0227af2242af
Brain Philemon <philbrainy@gmail.com>
2022-11-23 23:31:34 +0100

    feat: updated for all categories

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable,  } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { InvalidCategories } from 'src/common/enums/invalidCategories.enum';
 import { Visibility } from 'src/common/enums/visibility.enum';
@@ -7,7 +7,6 @@ import { Api } from 'src/entities/api.entity';
 import { Category } from 'src/entities/category.entity';
 import { Repository } from 'typeorm';
 import { CreateCategoryDto } from './dto/create-category.dto';
-
 
 @Injectable()
 export class CategoriesService {
@@ -18,20 +17,22 @@ export class CategoriesService {
     private readonly apiRepo: Repository<Api>,
   ) {}
 
-  async createNewCategory(createCategoryDto: CreateCategoryDto):Promise<Category> {
-    try{
+  async createNewCategory(
+    createCategoryDto: CreateCategoryDto,
+  ): Promise<Category> {
+    try {
       const category = await this.categoryRepo.findOne({
-        where: { name: createCategoryDto.name},
-      })
+        where: { name: createCategoryDto.name },
+      });
 
-      if(category) {
+      if (category) {
         throw new BadRequestException(
           ZaLaResponse.BadRequest(
             'Existing Value',
             `A category with name ${createCategoryDto.name} already exist`,
-            '400'
-          )
-        )
+            '400',
+          ),
+        );
       }
 
       const newCategory = this.categoryRepo.create({
@@ -39,62 +40,59 @@ export class CategoriesService {
       });
       const savedCategory = await this.categoryRepo.save(newCategory);
       return savedCategory;
-    }
-    catch(err){
+    } catch (err) {
       throw new BadRequestException(
-        ZaLaResponse.BadRequest(err.name, err.message, err.status)
-      )
+        ZaLaResponse.BadRequest(err.name, err.message, err.status),
+      );
     }
   }
 
   async findAllCategory(): Promise<Category[]> {
-    try{
-      const category = await this.categoryRepo.find()
+    try {
+      const category = await this.categoryRepo.find();
       return category;
-    }
-    catch(err){
+    } catch (err) {
       throw new BadRequestException(
-        ZaLaResponse.BadRequest(err.name, err.message, err.status)
-      )
+        ZaLaResponse.BadRequest(err.name, err.message, err.status),
+      );
     }
   }
 
-
-  async getAllApis(categoryId: string){
+  async getAllApis(categoryId: string) {
     try {
-      const apis = await this.apiRepo.find({where:{categoryId: categoryId, visibility: Visibility.Public}})     
-      return apis
-      
+      const category = await this.categoryRepo.findOne({
+        where: { id: categoryId },
+      });
+      if (category.name.toLowerCase() == 'all')
+        return await this.apiRepo.find({
+          where: { visibility: Visibility.Public },
+        });
+      const apis = await this.apiRepo.find({
+        where: { categoryId, visibility: Visibility.Public },
+      });
+      return apis;
     } catch (error) {
       throw new BadRequestException(
-        ZaLaResponse.BadRequest(
-          error.name,
-          error.message,
-          error.status
-        )
-      )
+        ZaLaResponse.BadRequest(error.name, error.message, error.status),
+      );
     }
   }
 
-  async deleteCategory(categoryId: string, generalCateroryId: string){
+  async deleteCategory(categoryId: string, generalCateroryId: string) {
     try {
       //move all apis in this category to a general category section
-      await this.apiRepo.createQueryBuilder()
+      await this.apiRepo
+        .createQueryBuilder()
         .update()
-        .set({categoryId: generalCateroryId})
-        .where('categoryId = :categoryId', {categoryId})
-        .execute()
+        .set({ categoryId: generalCateroryId })
+        .where('categoryId = :categoryId', { categoryId })
+        .execute();
 
-      await this.categoryRepo.delete(categoryId)
-      
+      await this.categoryRepo.delete(categoryId);
     } catch (error) {
       throw new BadRequestException(
-        ZaLaResponse.BadRequest(
-          error.name,
-          error.message,
-          error.status
-        )
-      )
+        ZaLaResponse.BadRequest(error.name, error.message, error.status),
+      );
     }
   }
 
@@ -102,27 +100,26 @@ export class CategoriesService {
    * it gets a list of all the categories that apis can be added to
    * @returns a lis of categories
    */
-  async getAllValidCategories(): Promise<Category[]>{
-    try{
-      const categories = await this.categoryRepo.find()
-      const invalidCategories = Object.values(InvalidCategories)
-      
+  async getAllValidCategories(): Promise<Category[]> {
+    try {
+      const categories = await this.categoryRepo.find();
+      const invalidCategories = Object.values(InvalidCategories);
+
       // loop through categories list and invalid categories list
-      invalidCategories.forEach(invalidCategory=>{
-        categories.forEach(category=>{
-          // remove category if it can be found in the invalid categories enum 
-          if(invalidCategory === category.name){
-            const index = categories.indexOf(category)
-            categories.splice(index, 1)
+      invalidCategories.forEach((invalidCategory) => {
+        categories.forEach((category) => {
+          // remove category if it can be found in the invalid categories enum
+          if (invalidCategory === category.name) {
+            const index = categories.indexOf(category);
+            categories.splice(index, 1);
           }
-        })
-      })
-      return categories
-    }
-    catch(err){
+        });
+      });
+      return categories;
+    } catch (err) {
       throw new BadRequestException(
-        ZaLaResponse.BadRequest(err.name, err.message, err.status)
-      )
+        ZaLaResponse.BadRequest(err.name, err.message, err.status),
+      );
     }
   }
 }
