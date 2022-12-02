8a2835f37b66f880109af8a92692edf7e7f180c7
Olasunmade Babafemi <babafemiolasunmade@gmail.com>
2022-12-02 13:10:22 +0100

    accept invitation and notify api author

--- src/invitation/invitation.service.ts
+++ src/invitation/invitation.service.ts
@@ -50,7 +50,17 @@ export class InvitationService {
       const invitationSecret = process.env.INVITATION_SECRET
       const invitationExpiry = process.env.INVITATION_EXPIRY
       const invitee = await this.profileRepo.findOne({where:{email: createInvitationDto.inviteeEmail}})
+      const currentContributors = api.contributors
 
+      if (currentContributors.includes(invitee.id)){
+        throw new BadRequestException(
+          ZaLaResponse.BadRequest(
+            "Bad Request Error",
+            "User already a contributor to this API",
+            "400"
+          )
+        )
+      }
       if(!invitee){
         throw new NotFoundException(
           ZaLaResponse.NotFoundRequest(
@@ -67,45 +77,33 @@ export class InvitationService {
       const token = await this.jwTokenService.signAsync(
         jwtPayload, {secret: invitationSecret, expiresIn: invitationExpiry}
       )
-      if (existingInvite){
-        verify = this.jwTokenService.verify(existingInvite.token,{secret: invitationSecret})       
-        if(!verify){
-            newInvite = this.invitationRepo.create({
-            apiId: api.id,
-            apiAuthor: api.profileId,
-            inviteeEmail: invitee.email,
-            inviteeId: invitee.id,
-            token: token
-          })
-          await this.invitationRepo.save(newInvite)
-        } else if(verify){
-          throw new BadRequestException(
-            ZaLaResponse.BadRequest(
-              "Bad Request Error",
-              "A Pending Invitation exists for this user",
-              "400"
-            )
-          )
-        }  
-      }   
+      
+      verify = existingInvite? this.jwTokenService.verify(existingInvite.token,{secret: invitationSecret}) : null
 
-      newInvite =  this.invitationRepo.create({
+      if(!verify){
+        newInvite = this.invitationRepo.create({
         apiId: api.id,
         apiAuthor: api.profileId,
         inviteeEmail: invitee.email,
         inviteeId: invitee.id,
         token: token
-
-      })
+        })
+        await this.invitationRepo.save(newInvite)
+      } else if(verify){
+        throw new BadRequestException(
+          ZaLaResponse.BadRequest(
+            "Bad Request Error",
+            "A Pending Invitation exists for this user",
+            "400"
+          )
+        )
+      }  
+     
       await this.invitationRepo.save(newInvite)
 
       const coreBaseUrl = this.configService.get<string>(configConstant.baseUrls.coreService)
       const acceptUrl = `${coreBaseUrl}/api/v1/invitation/accept/${api.id}/${invitee.id}`
-      const body = `
-        <h1>Hello ${invitee.email},</h1>
-        <h2> You have been invited to be a contributor to the ${api.name} API </h2>
-        <a href=${acceptUrl}> Click Here To Accept Invite</a>
-      `
+
       const rawText = `Hello ${invitee.email}, \n
       You have been invited to be a contributor to the ${api.name} API \n
         Click The Link Below To Accept Invite: \n${acceptUrl}
@@ -113,7 +111,6 @@ export class InvitationService {
       const mailData = {
         email: invitee.email,
         subject: `Invitation to Become A Contributor To An API`,
-        html: body,
         text: rawText,
       }
       await this.sendMail('mail', mailData)
@@ -122,9 +119,9 @@ export class InvitationService {
     } catch (error) {
       throw new BadRequestException(
         ZaLaResponse.BadRequest(
-          'Internal Server Error',
-          'Something went wrong',
-          '500',
+          error.response.error||'Internal Server Error',
+          error.response.message||'Something went wrong',
+          error.response.errorCode||'500',
         ),
       );
     }
@@ -142,7 +139,6 @@ export class InvitationService {
           apiId: api.id
         }
       })
-      
 
       if(!existingInvite){
         throw new NotFoundException(
@@ -153,6 +149,8 @@ export class InvitationService {
           )
         )
       }
+
+      const apiAuthor = await this.profileRepo.findOne({where:{id: api.profileId}})
       
       const invitationSecret = process.env.INVITATION_SECRET
       const contributors = api.contributors
@@ -160,7 +158,20 @@ export class InvitationService {
       //Check if the invitation has not expired
       const verify = await this.jwTokenService.verify(existingInvite.token,{secret: invitationSecret})
       contributors.push(verify.inviteeId)
-      
+
+      const textBody = `
+      Hello ${apiAuthor.email}, \n
+      ${existingInvite.inviteeEmail} has accepted your request to be a contributor to your API: \n
+      ${String(api.name).toUpperCase()}, \n Check your api contributors for confirmation.
+      `
+      const mailData = {
+        email: apiAuthor.email,
+        subject: `API contributor Request Response`,
+        text: textBody,
+      }
+
+      await this.sendMail('mail', mailData)
+
       await this.apiRepo.update(
         api.id,
         {contributors: contributors},
@@ -182,9 +193,9 @@ export class InvitationService {
       } else {
         throw new BadRequestException(
           ZaLaResponse.BadRequest(
-            'Internal Server Error',
-            'Something went wrong',
-            '500',
+            error.response.error||'Internal Server Error',
+            error.response.message||'Something went wrong',
+            error.response.errorCode||'500',
           ),
         );
       }
@@ -204,9 +215,9 @@ export class InvitationService {
     } catch (error) {
         throw new BadRequestException(
           ZaLaResponse.BadRequest(
-            'Internal Server Error',
-            'Something went wrong',
-            '500',
+            error.response.error||'Internal Server Error',
+            error.response.message||'Something went wrong',
+            error.response.errorCode||'500',
           ),
         );
     }
@@ -218,7 +229,11 @@ export class InvitationService {
         this.n_client.emit(pattern, payload);
       } catch (error) {
         throw new BadRequestException(
-          ZaLaResponse.BadRequest('Internal Server error', error.message, '500'),
+           ZaLaResponse.BadRequest(
+            error.response.error||'Internal Server Error',
+            error.response.message||'Something went wrong',
+            error.response.errorCode||'500',
+          ),
         );
       }
   }
